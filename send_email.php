3df2a85d919b9e316054d43e6684ad98b77da206
Mateuzinho3003 <113572331+Mateuzinho3003@users.noreply.github.com>
2024-07-09 00:59:20 -0300

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -2,9 +2,9 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require '/home/storage/5/ee/a5/dinesh0006tested1/public_html/PHPMailer/src/Exception.php';
-require '/home/storage/5/ee/a5/dinesh0006tested1/public_html/PHPMailer/src/PHPMailer.php';
-require '/home/storage/5/ee/a5/dinesh0006tested1/public_html/PHPMailer/src/SMTP.php';
+require 'caminho_completo_pwd/public_html/PHPMailer/src/Exception.php';
+require 'caminho_completo_pwd/public_html/PHPMailer/src/PHPMailer.php';
+require 'caminho_completo_pwd/public_html/PHPMailer/src/SMTP.php';
 
 // Instancia o PHPMailer
 $mail = new PHPMailer(true);
@@ -12,11 +12,11 @@ $mail = new PHPMailer(true);
 try {
     // Configurações do servidor
     $mail->isSMTP();                                          // Enviar usando SMTP
-    $mail->Host       = 'email-ssl.com.br';           // Servidor SMTP
-    $mail->SMTPAuth   = true;    // Ativar autenticação SMTP
-    $mail->Username   = 'csv@testedeaplicacao.com';            // Nome de usuário SMTP
-    $mail->Password   = 'Locaweb@@10203040';                  // Senha SMTP
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;          // Criptografia SMTPS
+    $mail->Host       = 'email-ssl.com.br';                   // Servidor SMTP
+    $mail->SMTPAuth   = true;                                 // Ativar autenticação SMTP
+    $mail->Username   = 'usuario@dominio';                    // Nome de usuário SMTP
+    $mail->Password   = 'senha-do-email';                     // Senha SMTP
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;       // Criptografia SMTPS
     $mail->Port       = 587;                                  // Porta TCP para conexão
     $mail->SMTPDebug  = 2;                                    // Nível de debug
 
@@ -24,8 +24,8 @@ try {
     $mail->AuthType = 'PLAIN';                                // Tipo de autenticação
 
     // Destinatários
-    $mail->setFrom('csv@testedeaplicacao.com', 'Teste Form');
-    $mail->addAddress('csv@testedeaplicacao.com', 'Destinatário');
+    $mail->setFrom('usuario@dominio', 'Teste Form');
+    $mail->addAddress('usuario@dominio', 'Destinatário');
 
     // Conteúdo
     $mail->isHTML(true);                                      // Formato HTML
